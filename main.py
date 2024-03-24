d3010c52222b9daef35c29da2bdcc971ad418699
Ahmed A. El-Otify <115142044+3b3z-himself@users.noreply.github.com>
2024-03-24 19:03:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class ConnectX:
         options = webdriver.ChromeOptions()
         # options.add_argument('--no-sandbox')
         # options.add_argument('--disable-dev-shm-usage')
-        # options.add_argument('--disable-gpu')
+        options.add_argument('--disable-gpu')
         options.add_argument("--incognito")
         options.add_argument("--start-maximized")
         options.add_argument('--log-level=3')
